f0a78f364f3770f91096056cec3037173044148e
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-11-11 02:13:25 -0800

    updated restricted file view to no longer redirect

--- public/views.js
+++ public/views.js
@@ -182,7 +182,7 @@ let show_image = function(filename, public) {
     `
     <div class="row">
         <div class="col-sm-8 align-items-center">
-            <p name="${window.location.origin}/${username}/${filename}">${filename}</p>
+            <a class="link" onclick="window.open('${window.location.origin}/view/${filename}', '_blank');">${filename}</a>
         </div>
         
         <div class="col-sm-2">
